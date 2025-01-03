ab9762ebba14684b5ea77e59493be79846aaf3e7
Albert Diones <adiones@addteam.org>
2025-01-03 18:00:54 +0800

    fetch tickers data

--- index.ts
+++ index.ts
@@ -1,7 +1,28 @@
 
 import type { AssetHolding, AssetWallet, CandleFetcher, Exchange, TickerFetcher } from 'tradeexchanges';
 import type { TickerData } from 'tradeexchanges/tradingCandles';
-import HttpClient from 'nonChalantJs';
+import HttpClient, { type ResponseDataWithCache } from 'nonChalantJs';
+
+interface BybitResponse {
+    result: {
+        list: BybitTickerData[]
+    }
+}
+
+interface BybitTickerData {
+    symbol: string;
+    bid1Price: string;
+    bid1Size: string;
+    ask1Price: string;
+    ask1Size: string;
+    lastPrice: string;
+    prevPrice24h: string;
+    price24hPcnt: string;
+    highPrice24h: string;
+    lowPrice24h: string;
+    turnover24h: string;
+    volume24h: string;
+}
 
 
 export class Bybit implements TickerFetcher {
@@ -16,9 +37,16 @@ export class Bybit implements TickerFetcher {
         this.options = options;
     }
 
-    _getMarketTickers() {
+    _getMarketTickers(): Promise<{response: BybitResponse, fromCache: boolean}> {
         return this.client.getWithCache(
             'https://api.bybit.com/v5/market/tickers?category=spot'
+        ).then(
+            (fetchResponse: ResponseDataWithCache) => {
+                return {
+                    response: fetchResponse.response as BybitResponse,
+                    fromCache: fetchResponse.fromCache
+                }
+            }
         );
     }
 
@@ -37,24 +65,27 @@ export class Bybit implements TickerFetcher {
     }
 
     async getTickerData(symbol: string): Promise<{data: TickerData,fromCache: Boolean} | null> {
-        return {
-            data: {
-                "symbol": "BTC",
-                "current": 96942.81,
-                "high": 97739.82,
-                "low": 95982.19,
-                "base_volume": 39750000000,
-                "quote_volume": 1919944500581.36,
-                "tags": ["store-of-value", "pow", "decentralized"],
-                "circulating_supply": 19804918,
-                "status": "active",
-                "full_data": {
-                  "market_cap": 1919944500581.36,
-                  "total_supply": 21000000,
-                  "last_updated": "2025-01-03T00:00:00Z"
+        
+        return this._getMarketTickers().then(
+                ({response}) => {
+                    console.log(response);
+                    return {
+                        data: response.result.list.filter(
+                            (ticker: {symbol: string }) => ticker.symbol === symbol
+                        )?.map(
+                            (ticker): TickerData => ({
+                                symbol: ticker.symbol,
+                                current: parseFloat(ticker.lastPrice),
+                                high: parseFloat(ticker.highPrice24h),
+                                low: parseFloat(ticker.lowPrice24h),
+                                base_volume: parseFloat(ticker.volume24h),
+                                quote_volume: parseFloat(ticker.volume24h)*parseFloat(ticker.lastPrice),
+                                full_data: ticker
+                            })
+                        )[0],
+                        fromCache: false
+                    }
                 }
-            },
-            fromCache: false
-        };
+            );
     }
 }
